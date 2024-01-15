2a58b3720cecc5c017ccbf2892846054c6284508
Leonardo Pedroso dos Santos <leonardopedroso27@yahoo.com.br>
2024-01-14 23:01:31 -0300

    Weeks left to X years

---
+++ Weeks left 2.py
@@ -0,0 +1,15 @@
+print("***Weeks left to X years***\n")
+Age = int(input("How old are you?\n"))
+Years = int(input("Choose the age you want:\n"))
+
+Calculate = Years - Age
+
+OneYear = 52.1786
+
+Calculate2 = Calculate * OneYear
+
+Result = Calculate
+
+print(f"\nYou have {round(Result ,2)} weeks left to {Years} years")
+
+print(input("\nClick ENTER to close the window"))
